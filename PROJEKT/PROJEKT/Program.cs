49d726cc980c4db198c26573b0037bf967d83a74
wojtek230689 <wojtekkawlewski@gmail.com>
2020-12-10 22:05:01 +0100

    ..

--- PROJEKT/PROJEKT/Program.cs
+++ PROJEKT/PROJEKT/Program.cs
@@ -1,4 +1,14 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml;
+using PROJEKT.Classes;
+using PROJEKT.Classes.Database.Objects;
+using PROJEKT.Classes.Messages;
+using PROJEKT.Classes.Exceptions;
+using PROJEKT.Classes.System;
 
 namespace PROJEKT
 {
@@ -6,8 +16,43 @@ namespace PROJEKT
     {
         static void Main(string[] args)
         {
-            Menu menu = new Menu();
-            menu.menuUsera();
-         }
+            Console.Clear();
+
+            Log.CurrentLevel = Log.LevelEnum.DEB;
+
+
+            using var _log = Log.DEB("Program", "Main");
+
+            _log.PR_DEB("Początek aplikacji");
+
+
+            if ((args?.Length ?? 0) < 1)
+            {
+                _log.PR_DEB("Brak argumentu uruchomieniowego! 1 - serwer, 2 - klient");
+
+            }
+            else
+            {
+                int.TryParse(args[0], out int _iMode);
+
+                XmlStorageTypes.Register<Exception>();
+
+                MessageFactory.Instance.Register<LoginMessage>();
+                MessageFactory.Instance.Register<TextMessage>();
+
+                switch (_iMode)
+                {
+                    case 1:
+                        new TestServer().Run(); break;
+
+                    case 2:
+                        new TestClient().Run(); break;
+
+                }
+
+            }
+
+
+        }
     }
 }
---
+++ PROJEKT/PROJEKT/TestClient.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+using PROJEKT.Classes.Services;
+using PROJEKT.Classes;
+using PROJEKT.Interfaces;
+
+using static PROJEKT.Interfaces.INetworkAction;
+using System.Threading.Tasks;
+using PROJEKT.Classes.Messages;
+using System.IO;
+
+namespace PROJEKT
+{
+    public class TestClient : INetworkAction
+    {
+        private static readonly object LOCKOBJECT = new object();
+
+        public ClientService Client;
+
+        public bool StillWorking = true;
+
+        public void StateChanged(State a_eState, StateObject a_oStateObj = null)
+        {
+            lock (LOCKOBJECT)
+            {
+
+                switch (a_eState)
+                {
+                    case State.Sending:
+                        break;
+
+                    case State.Sent:
+                        break;
+
+                    case State.Connecting:
+                        break;
+
+                    case State.Connected:
+                        OnConnected(a_oStateObj);
+
+                        break;
+
+                    case State.Receiving:                        
+                        break;
+
+                    case State.Received:
+                        OnReceived(a_oStateObj);
+                        break;
+
+                    case State.Error:
+                        break;
+                }
+            }
+        }
+
+        protected void OnReceived(StateObject a_oStateObj)
+        {
+            var _client = a_oStateObj.GetObject<ClientService>();
+
+            var _message = MessageFactory.Instance.Create(_client.Data.BufferWithData);
+
+            try
+            {
+                _message.ProcessResponse(a_oStateObj);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Wystąpił błąd! {e.Message}");
+            }
+
+            _client.AsyncReceive();
+        }
+
+        protected void OnConnected(StateObject a_oStateObj)
+        {
+            var _client = a_oStateObj.GetObject<ClientService>();
+
+            Console.Write("Podaj login:");
+
+            var _loginTelegram = new LoginMessage
+            {
+                Login = Console.ReadLine()
+            };
+
+            _client.AsyncSend(_loginTelegram.AsNetworkData());
+
+            OnReceived(_client.SyncReceive());
+        }
+        public virtual void MenuDisplay()
+        {
+            Console.WriteLine();
+            Console.WriteLine("1 - zaloguj");
+            Console.WriteLine("2 - wyślij wiadomość do wszystkich");
+            Console.WriteLine("3 - wyślij wiadomość do użytkownika");
+            Console.WriteLine("0 - wyjdź");
+            Console.WriteLine();
+        }
+
+        public virtual void SendMessage(bool a_bToAll)
+        {
+            Console.WriteLine("Wprowadz dane:");
+            string _sTo = "*";
+
+            if (!a_bToAll)
+            {
+                Console.Write("Do:");
+                _sTo = Console.ReadLine();
+            }
+
+            Console.Write("Wiadomosc:");
+            string _sText = Console.ReadLine();
+
+            TextMessage _msgTo = new TextMessage
+            {
+                From = Client.Identifier,
+                To = _sTo,
+                Text = _sText
+            };
+
+            Client.AsyncSend(_msgTo.AsNetworkData());
+        }
+
+        public virtual void Run()
+        {
+            Client = new ClientService(IPAddress.Loopback, 1000)
+            {
+                NetworkAction = this
+            };
+
+            MenuDisplay();
+
+            while (StillWorking)
+            {
+                if (Console.KeyAvailable)
+                {
+                    switch (Console.ReadKey(true).Key)
+                    {
+                        case ConsoleKey.D0:
+                            StillWorking = false; break;
+
+                        case ConsoleKey.D1:
+                            if (!Client.IsConnected)
+                                Client.Establish();
+                            break;
+
+                        case ConsoleKey.D2:
+                            SendMessage(true);
+                            break;
+
+                        case ConsoleKey.D3:
+                            SendMessage(false);
+                            break;
+
+                    }
+                }
+                else
+                    Thread.Sleep(10);
+            }
+        }
+    }
+}
